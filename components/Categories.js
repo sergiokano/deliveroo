a412982732d0b317a750b712a27c720ab21c3559
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-04 13:59:49 +0100

    feat: create search bar and header in homescreen

---
+++ components/Categories.js
@@ -0,0 +1,13 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <ScrollView>
+        {/* CategoryCard */}
+      <Text>Categories</Text>
+    </ScrollView>
+  )
+}
+
+export default Categories
