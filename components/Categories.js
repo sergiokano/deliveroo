11f44fe6e9e89bd4d69fcccaff1108aa53f8f8ff
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-04 14:06:24 +0100

    feat: create and show categories mock

--- components/Categories.js
+++ components/Categories.js
@@ -1,13 +1,27 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
   return (
-    <ScrollView>
-        {/* CategoryCard */}
+    <ScrollView
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+      }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    >
+      {/* CategoryCard */}
+      <CategoryCard imgUrl="https://links.papareact.com/wru" title="TESTING" />
+      <CategoryCard imgUrl="https://links.papareact.com/wru" title="TESTING" />
+      <CategoryCard imgUrl="https://links.papareact.com/wru" title="TESTING" />
+      <CategoryCard imgUrl="https://links.papareact.com/wru" title="TESTING" />
+      <CategoryCard />
+      <CategoryCard />
       <Text>Categories</Text>
     </ScrollView>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
