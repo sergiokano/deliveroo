040806809557834eaf0743612f75df513822e276
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-04 17:47:40 +0100

    feat: link sanity correctly

---
+++ sanity.js
@@ -0,0 +1,17 @@
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from '@sanity/image-url'
+
+const client = sanityClient({
+  projectId: "n9klddin",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2022-03-07",
+});
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
+
+// RUN THIS to add exception for localhost 3000 CORS Policy
+// sanity cors add http://localhost:3000
+
+export default client;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,7 +6,7 @@ import {
   TextInput,
   ScrollView,
 } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useColorScheme } from "nativewind";
 import {
@@ -17,15 +17,35 @@ import {
 } from "react-native-heroicons/outline";
 import Categories from "../components/Categories";
 import FeaturedRow from "../components/FeaturedRow";
+import sanityClient from "../sanity";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, []);
-  const { colorScheme } = useColorScheme();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `
+          *[_type == "featured"]  {
+            ...,
+            restaurants[]->{
+              ...
+              dishes[]->{
+              }
+            }
+         `
+      )
+      .then((data) => {
+        setFeaturedCategories(data);
+      });
+  }, []);
 
   return (
     <SafeAreaView className="bg-white pt-5">
