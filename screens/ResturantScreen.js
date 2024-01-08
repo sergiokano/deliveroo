dc5ada772cee2daa9356d53ae1ac4c183a52aee0
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-08 11:45:18 +0100

    feat: add header restaurant screen details

--- screens/ResturantScreen.js
+++ screens/ResturantScreen.js
@@ -2,7 +2,16 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { urlFor } from "../sanity";
-import { ArrowLeftIcon } from "react-native-heroicons/solid";
+import {
+  ArrowLeftIcon,
+  MapPinIcon,
+  PlayCircleIcon,
+  StarIcon,
+} from "react-native-heroicons/solid";
+import {
+  ChevronRightIcon,
+  QuestionMarkCircleIcon,
+} from "react-native-heroicons/outline";
 
 const ResturantScreen = () => {
   const navigation = useNavigation();
@@ -35,10 +44,46 @@ const ResturantScreen = () => {
           source={{ uri: urlFor(imgUrl).url() }}
           className="w-full h-56 bg-gray-300 p-4"
         />
-        <TouchableOpacity  onPress={navigation.goBack} className=" absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
+        <TouchableOpacity
+          onPress={navigation.goBack}
+          className=" absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
+        >
           <ArrowLeftIcon size={20} color="#00CCBB" />
         </TouchableOpacity>
       </View>
+
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+          <Text className="text-3xl font-bold">{title}</Text>
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{rating}</Text> · {genre}
+              </Text>
+            </View>
+            <View className="flex-row items-center space-x-1 flex-shrink">
+              <MapPinIcon color="gray" opacity={0.4} size={22} />
+              <Text
+                className="text-xs text-gray-500"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                <Text className="text-green-500">Nearby</Text> · {address}
+              </Text>
+            </View>
+          </View>
+          <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+        </View>
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text className="pl-2 flex-1 text-md font-bold">
+            Have a food allergy?
+          </Text>
+          <ChevronRightIcon color="#00CCBB" size={20} />
+        </TouchableOpacity>
+      </View>
+      
     </ScrollView>
   );
 };
