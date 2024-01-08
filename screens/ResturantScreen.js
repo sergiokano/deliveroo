c0ce2cd3a152d8d570ce0bd189dcca377542660a
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-08 10:46:33 +0100

    feat: add goBack in restaurant

--- screens/ResturantScreen.js
+++ screens/ResturantScreen.js
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { urlFor } from "../sanity";
+import { ArrowLeftIcon } from "react-native-heroicons/solid";
 
 const ResturantScreen = () => {
-  return (
-    <View>
-      <Text>ResturantScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default ResturantScreen
+  const {
+    params: {
+      id,
+      imgUrl,
+      title,
+      rating,
+      genre,
+      address,
+      short_description,
+      dishes,
+      long,
+      lat,
+    },
+  } = useRoute();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <ScrollView>
+      <View>
+        <Image
+          source={{ uri: urlFor(imgUrl).url() }}
+          className="w-full h-56 bg-gray-300 p-4"
+        />
+        <TouchableOpacity  onPress={navigation.goBack} className=" absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
+          <ArrowLeftIcon size={20} color="#00CCBB" />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default ResturantScreen;
