d57e35534bcfbcffeea849684dfc6dc130c0850c
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-17 19:25:28 +0100

    feat: create Basket screen and route

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -10,17 +10,19 @@ const BasketIcon = () => {
   const basketTotal = useSelector(selectBasketTotal);
 
   return (
-    <View className='absolute bottom-10 w-full z-50'>
-      <TouchableOpacity className='mx-5 bg-[#00CCBB] p-4 rounded-lg flex-row items-center space-x-1' >
+    <View className="absolute bottom-10 w-full z-50">
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Basket")}
+        className="mx-5 bg-[#00CCBB] p-4 rounded-lg flex-row items-center space-x-1"
+      >
         <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">
-            {items.length} 
-
+          {items.length}
         </Text>
-        <Text className="flex-1 text-center font-extrabold text-lg text-white" >
-            View Basket
+        <Text className="flex-1 text-center font-extrabold text-lg text-white">
+          View Basket
         </Text>
         <Text className="text-lg text-white font-extrabold">
-            {basketTotal} €
+          {basketTotal} €
         </Text>
       </TouchableOpacity>
     </View>
