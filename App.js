7454d1ddf98923eb0d583e4b10b4ce5d66b42a62
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-17 17:12:54 +0100

    feat: install redux and apply to the project

--- App.js
+++ App.js
@@ -4,16 +4,20 @@ import { Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import ResturantScreen from "./screens/ResturantScreen";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Resturant" component={ResturantScreen} />
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Resturant" component={ResturantScreen} />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
