122c51452c2f4c96c82666e480d06bb9c3125da0
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-17 19:40:05 +0100

    feat: add restaurantslice

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,7 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { selectRestaurant } from '../features/restaurantSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectBasketItems } from '../features/basketSlice';
 
 const BasketScreen = () => {
+    const navigation = useNavigation();
+    const restaurant = useSelector(selectRestaurant);
+    const items = useSelector(selectBasketItems);
+    const dispatch = useDispatch();
+
   return (
     <View>
       <Text>BasketScreen</Text>
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { urlFor } from "../sanity";
 import {
@@ -14,9 +14,12 @@ import {
 } from "react-native-heroicons/outline";
 import DishRow from "../components/DishRow";
 import BasketIcon from "../components/BasketIcon";
+import { useDispatch } from "react-redux";
+import { setRestaurant } from "../features/restaurantSlice";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   const {
     params: {
@@ -33,6 +36,23 @@ const RestaurantScreen = () => {
     },
   } = useRoute();
 
+  useEffect(() => {
+    dispatch(
+      setRestaurant({
+        id,
+        imgUrl,
+        title,
+        rating,
+        genre,
+        address,
+        short_description,
+        dishes,
+        long,
+        lat,
+      })
+    );
+  }, [dispatch]);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -41,7 +61,7 @@ const RestaurantScreen = () => {
 
   return (
     <>
-    <BasketIcon/>
+      <BasketIcon />
       <ScrollView>
         <View>
           <Image
