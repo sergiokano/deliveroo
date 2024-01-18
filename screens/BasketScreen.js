8e27c081178e3432bd355bb61e222f9e62c3fa1b
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-18 16:17:19 +0100

    feat: basketIcon dissapear when items === 0

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,21 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native';
-import { selectRestaurant } from '../features/restaurantSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectBasketItems } from '../features/basketSlice';
+import { View, Text } from "react-native";
+import React, { useMemo, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { selectRestaurant } from "../features/restaurantSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { selectBasketItems } from "../features/basketSlice";
 
 const BasketScreen = () => {
-    const navigation = useNavigation();
-    const restaurant = useSelector(selectRestaurant);
-    const items = useSelector(selectBasketItems);
-    const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
+  const items = useSelector(selectBasketItems);
+  const dispatch = useDispatch();
+  const [groupedItemsInBasket, setGroundedItemsInBasket] = useState([]);
+
+  useMemo(() => {}, [items]);
 
   return (
     <View>
       <Text>BasketScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default BasketScreen
+export default BasketScreen;
