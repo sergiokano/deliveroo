122cd520760fc094058912b965ae1c15928f704e
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-19 14:38:37 +0100

    feat: add X Circle to basket screen

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,9 +1,15 @@
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import React, { useMemo, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { selectRestaurant } from "../features/restaurantSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { selectBasketItems } from "../features/basketSlice";
+import { XCircleIcon } from "react-native-heroicons/solid";
 
 const BasketScreen = () => {
   const navigation = useNavigation();
@@ -16,14 +22,28 @@ const BasketScreen = () => {
     const groupedItems = items.reduce((results, item) => {
       (results[item.id] = results[item.id] || []).push(item);
       return results;
-    },
-    {});
-    setGroundedItemsInBasket( groupedItems);
+    }, {});
+    setGroundedItemsInBasket(groupedItems);
   }, [items]);
 
   return (
     <SafeAreaView>
-      <Text>BasketScreen</Text>
+      <View>
+        <View>
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">
+              {restaurant.title}
+            </Text>
+          </View>
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 absolute top-3 right-5"
+          >
+            <XCircleIcon color="#00CCBB" height={50} width={50} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
