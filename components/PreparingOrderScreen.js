4d57ac5c851cc0928b7b1172ec0d296568342d49
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-19 17:37:21 +0100

    feat: add loading icon to preparing order screen

--- components/PreparingOrderScreen.js
+++ components/PreparingOrderScreen.js
@@ -1,9 +1,20 @@
 import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import * as Animatable from "react-native-animatable";
+import * as Progress from "react-native-progress";
 import foodDelivery from "../assets/buildCity.gif";
+import { useNavigation } from "@react-navigation/native";
 
 const PreparingOrderScreen = () => {
+    const navigation = useNavigation();
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         navigation.navigate("Delivery");
+    //     }, 4000);
+    // }, []);
+
+
   return (
     <SafeAreaView className="bg-[#e6e6e6] justify-center items-center flex-1">
       <Animatable.Image
@@ -12,6 +23,15 @@ const PreparingOrderScreen = () => {
         iterationCount={1}
         className="h-80 w-full"
       />
+
+      <Animatable.Text
+        animation="slideInUp"
+        iterationCount={1}
+        className="text-lg my-10 text-[#00CCBB] font-bold text-center"
+      >
+        Waiting for Restaurant to confirm your order...
+      </Animatable.Text>
+      <Progress.Circle size={30} indeterminate={true} borderWidth={1} color="#00CCBB" />
     </SafeAreaView>
   );
 };
