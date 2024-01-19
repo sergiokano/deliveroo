915c5e0f9e6c9749a3392462cd555d0342f1d789
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-19 18:50:05 +0100

    feat: add maps and details to delivery screen

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -5,16 +5,23 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useSelector } from "react-redux";
 import { selectRestaurant } from "../features/restaurantSlice";
 import { XMarkIcon } from "react-native-heroicons/solid";
 import * as Progress from "react-native-progress";
+import MapView, { Marker } from "react-native-maps";
+import { enableLatestRenderer } from "react-native-maps";
+
+useEffect(() => {
+  enableLatestRenderer();
+}, []);
 
 const DeliveryScreen = () => {
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurant);
+
   return (
     <View className="bg-[#00CCBB] flex-1">
       <SafeAreaView className="z-50 mt-8">
@@ -43,6 +50,41 @@ const DeliveryScreen = () => {
             Your order at {restaurant.title} is being prepared
           </Text>
         </View>
+      </SafeAreaView>
+
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.long,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        className="flex-1 -mt-10 z-0"
+        mapType="standard"
+      >
+        <Marker
+          coordinate={{
+            latitude: restaurant.lat,
+            longitude: restaurant.long,
+          }}
+          title={restaurant.title}
+          description={restaurant.short_description}
+          identifier="origin"
+          pinColor="#00CCBB"
+        />
+      </MapView>
+      <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+        />
+        <View className="flex-1">
+          <Text className="text-lg">Sergio Cano</Text>
+          <Text className="text-gray-400">Your Rider</Text>
+        </View>
+        <Text className="text-[#00CCBB] text-lg mr-5 font-bold">Call</Text>
       </SafeAreaView>
     </View>
   );
