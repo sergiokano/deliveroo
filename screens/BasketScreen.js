5e893849980ce1f4debd0411ce50ca8b4c12b170
Sergio Cano <sergiocano.dev@gmail.com>
2024-01-19 14:54:32 +0100

    feat: add deliver time header in basket

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -3,6 +3,7 @@ import {
   Text,
   SafeAreaView,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import React, { useMemo, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -27,9 +28,9 @@ const BasketScreen = () => {
   }, [items]);
 
   return (
-    <SafeAreaView>
-      <View>
-        <View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b border-[#00CCBB] bg-white shadow-xs">
           <View>
             <Text className="text-lg font-bold text-center">Basket</Text>
             <Text className="text-center text-gray-400">
@@ -41,6 +42,18 @@ const BasketScreen = () => {
             className="rounded-full bg-gray-100 absolute top-3 right-5"
           >
             <XCircleIcon color="#00CCBB" height={50} width={50} />
+          </TouchableOpacity>
+        </View>
+        <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7  bg-gray-300 p-4 rounded-full"
+          />
+          <Text className="flex-1">Deliver in 50-75 min</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00CCBB]">Change</Text>
           </TouchableOpacity>
         </View>
       </View>
